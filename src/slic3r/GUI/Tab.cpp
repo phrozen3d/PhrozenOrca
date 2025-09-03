7f654e8fed26ecc45f54ec2e1cd8c2188df41bbc
SoftFever <softfeverever@gmail.com>
2025-09-03 22:16:31 +0800

    [Feature] Add a new feature that allow user to insert extra solid infills (#10611)
    * refactor Infill rotation template
    
    * clean up comments
    
    * set default solid_infill_rotate_template to empty
    
    * Fix an issue that infill_direction solid_infill_direction not working as expected
    
    * Add Extra Solid Infill Feature
    Introduced a new feature to insert extra solid infills at specific layers for enhanced strength in 3D prints.
    
    * fix doc error
    
    * fix image name
    
    * support "#K" for Explicit Layer List
    
    * update wiki

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -2263,7 +2263,8 @@ void TabPrint::build()
         optgroup->append_single_option_line("infill_wall_overlap", "strength_settings_infill#infill-wall-overlap");
 
         optgroup = page->new_optgroup(L("Advanced"), L"param_advanced");
-        optgroup->append_single_option_line("align_infill_direction_to_model");
+        optgroup->append_single_option_line("align_infill_direction_to_model", "strength_settings_advanced#align-infill-direction-to-model");
+        optgroup->append_single_option_line("extra_solid_infills", "strength_settings_infill#extra-solid-infill");
         optgroup->append_single_option_line("bridge_angle", "strength_settings_advanced#bridge-infill-direction");
         optgroup->append_single_option_line("internal_bridge_angle", "strength_settings_advanced#bridge-infill-direction"); // ORCA: Internal bridge angle override
         optgroup->append_single_option_line("minimum_sparse_infill_area", "strength_settings_advanced#minimum-sparse-infill-threshold");
