aab07afe2e030e1bfefd15daee47899f67c82091
Tim.Chen <tim.chen@phrozen3d.com>
2026-03-26 16:36:33 +0800

    fix: 修正支撐參數的圖示錯誤

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -6766,14 +6766,14 @@ void TabSLAPrint::build()
     // Support
     page = add_options_page(L("Support"), "custom-gcode_speed"); // ORCA: icon only visible on placeholders
 
-    optgroup = page->new_optgroup(L("Top"), L"PhrozenImages_Resin/param_main", 15);
+    optgroup = page->new_optgroup(L("Top"), L"PhrozenImages_Resin/param_top", 15);
     optgroup->append_single_option_line("contact_type", "123");
     optgroup->append_single_option_line("support_point_diameter", "123");
     optgroup->append_single_option_line("top_upper_diameter", "123");
     optgroup->append_single_option_line("top_contact_depth", "123");
     optgroup->append_single_option_line("pinhead_width", "123");
 
-    optgroup = page->new_optgroup(L("Main"), L"PhrozenImages_Resin/param_bottom", 15);
+    optgroup = page->new_optgroup(L("Main"), L"PhrozenImages_Resin/param_main", 15);
     optgroup->append_single_option_line("pillar_diameter", "123");
     optgroup->append_single_option_line("support_bracing_angle", "123");
 
@@ -6782,13 +6782,13 @@ void TabSLAPrint::build()
     optgroup->append_single_option_line("support_boss_height", "123");
 
 
-    optgroup = page->new_optgroup(L("Raft Setting"), L"PhrozenImages_Resin/param_bottom", 15);
+    optgroup = page->new_optgroup(L("Raft Setting"), L"PhrozenImages_Resin/param_raft_setting", 15);
     optgroup->append_single_option_line("pad_thickness_sla", "123");
     optgroup->append_single_option_line("pad_brim_size_sla", "123");
     optgroup->append_single_option_line("max_merge_distance_sla", "123");
     optgroup->append_single_option_line("pad_wall_slope_sla", "123");
 
-    optgroup = page->new_optgroup(L("Automatic Generation"), L"param_speed_first", 15);
+    optgroup = page->new_optgroup(L("Automatic Generation"), L"PhrozenImages_Resin/toolbar_support", 15);
     optgroup->append_single_option_line("support_points_density", "123");
     optgroup->append_single_option_line("max_bridge_length_sla", "123");
     optgroup->append_single_option_line("max_pillar_linking_distance", "123");
